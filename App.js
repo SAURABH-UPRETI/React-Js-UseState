eafe5bfc892e14963910a5cc913bfc69f881dcd3
Saurabh Upreti <83522982+SAURABH-UPRETI@users.noreply.github.com>
2021-09-09 17:00:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <TextForm heading="Enter the text to analyze bellow"/>
+      </div>
+    </> 
+  );
+}
+
+export default App;
